e94905f66f718e75c8e907bdae2375c9ef7400c6
ohpxho <lemuel.k.costuna.so@gmail.com>
2022-05-31 16:35:04 +0800

    final app version

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+let weather;
 
 //loader
 function configLoader(weather, loader){
@@ -23,6 +24,56 @@ document.querySelector('#srch-box').addEventListener('keyup', (event) => {
 //initial call
 document.body.onload = () => callFunctionOrder({});
 
+function toggleDegreeType(event){
+	let toggleBtn = document.querySelector('.degree-toggle');
+	let toggleWheel = document.querySelector('.toggle-wheel');
+	let toggleWheelSpan = document.querySelector('.toggle-wheel span');
+	if(event.target != toggleBtn){
+		return;	
+	}
+
+	toggleWheel.classList.toggle('toggle-wheel-translate');
+	let type = toggleWheelSpan.innerHTML;
+	toggleWheelSpan.innerHTML  = (type == '°C')? '°F' : '°C';
+
+	changeDegreeType((type == '°C')? 'fah': 'cel');
+}
+
+function changeDegreeType(type){
+	let currentCel = document.querySelector('.cel');
+	let currentFah = document.querySelector('.fah');
+	let cel = document.querySelectorAll('.text-cel');
+	let fah = document.querySelectorAll('.text-fah');
+	let celChart = document.querySelector('#celcius-chart');
+	let fahChart = document.querySelector('#fah-chart');
+	let celChartTranslatePosition = document.querySelector('#celcius-chart').style.transform;
+	let fahChartTranslatePosition = document.querySelector('#fah-chart').style.transform;
+	if(type == 'fah'){
+		currentCel.style.display = 'none';
+		currentFah.style.display = 'block';
+		cel[0].style.display = 'none';
+		fah[0].style.display = 'block';
+		cel[1].style.display = 'none';
+		fah[1].style.display = 'block';
+		cel[2].style.display = 'none';
+		fah[2].style.display = 'block';
+		celChart.style.cssText = `display: none !important; transform: ${celChartTranslatePosition}`;
+		fahChart.style.cssText = `display: block !important; transform: ${fahChartTranslatePosition}`;
+		return;
+	}
+
+	currentCel.style.display = 'block';
+	currentFah.style.display = 'none';
+	cel[0].style.display = 'block';
+	fah[0].style.display = 'none';
+	cel[1].style.display = 'block';
+	fah[1].style.display = 'none';
+	cel[2].style.display = 'block';
+	fah[2].style.display = 'none';
+	celChart.style.cssText = `display: block !important; transform: ${celChartTranslatePosition}`;
+	fahChart.style.cssText = `display: none !important; transform: ${fahChartTranslatePosition}`;
+}
+
 //API request
 async function weatherApiRequest(location){
 	let options = {
@@ -34,7 +85,13 @@ async function weatherApiRequest(location){
 	};
 	configLoader(false, true);
 	let response = await fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${location}&days=7`, options);
-	let result = await response.json();	
+	let result;
+	if(response.ok){
+		result = await response.json();	
+	}else{
+		alert('Location not found');
+		throw new Error('Location not found');
+	}
 	return result;
 }
 
@@ -70,11 +127,12 @@ function callFunctionOrder({loc = ''}){
 	if(loc == ''){
 		getHostGeolocation();
 	}else{
-		let weather = weatherApiRequest(loc).then(result => result);
+		weather = weatherApiRequest(loc).then(result => result);
 		displayWeatherInfo(weather); 
-		createChart(weather, 'temp', 0 , 0);
-		createChart(weather, 'humidity', 0, 1);
-		createChart(weather, 'wind', 0, 2);
+		createChart(weather, 'celcius', 0, 0);
+		createChart(weather, 'fah', 0, 1);
+		createChart(weather, 'humidity', 0, 2);
+		createChart(weather, 'wind', 0, 3);
 		getDaysToFollow(weather);
 	}
 }
@@ -82,7 +140,8 @@ function callFunctionOrder({loc = ''}){
 function displayWeatherInfo(weather){
 	weather.then(
 		(response) => {
-			let txtDegree = document.querySelector('.text-degree span');
+			let txtCelcius = document.querySelector('.text-degree .cel');
+			let txtFah = document.querySelector('.fah');
 			let txtPrecep = document.querySelector('.text-precep');
 			let txtHumidity = document.querySelector('.text-humidity');
 			let txtWind = document.querySelector('.text-wind');
@@ -90,7 +149,8 @@ function displayWeatherInfo(weather){
 			let txtDay = document.querySelector('.text-day');
 			let txtWeather = document.querySelector('.text-weather');
 			let weatherImg = document.querySelector('.text-degree img');
-			txtDegree.innerHTML  =response.current.feelslike_c + '°C';
+			txtCelcius.innerHTML  = response.current.feelslike_c + '°C';
+			txtFah.innerHTML  = response.current.feelslike_f + '°F';
 			weatherImg.src = response.current.condition.icon;
 			txtPrecep.innerHTML  = 'Precipation: ' + response.current.precip_in + '%';
 			txtHumidity.innerHTML  = 'Humidity: ' + response.current.humidity + '%';
@@ -109,8 +169,8 @@ let chart = []
 function createChart(weather, ctype, indx, cIndx){
 	weather.then((response)=>{
 		let chartData = setChartData(weather, ctype, indx);
-		let label = (ctype == 'temp')? 'Celcius' : (ctype == 'humidity')? 'Percent' : 'kph';  
-		let backColor = (ctype == 'temp')? '255, 204, 0, ' : '26, 115, 232, '; 
+		let label = (ctype == 'celcius')? 'Celcius' : (ctype == 'fah')? 'Fahrenheit': (ctype == 'humidity')? 'Percent' : 'kph';  
+		let backColor = (ctype == 'celcius' || ctype == 'fah')? '255, 204, 0, ' : '26, 115, 232, '; 
 		chartData.then(chartData => {
 			const ctx = document.getElementById(`${ctype}-chart`).getContext('2d');
 			if(chart[cIndx] != undefined) chart[cIndx].destroy();
@@ -169,13 +229,14 @@ function createChart(weather, ctype, indx, cIndx){
 		
 		});
 	});
+
 }
 
 
 function setChartData(weather, ctype, indx){
 	return weather.then((response) => {
 		let chartData = [];
-		if(ctype == 'temp'){
+		if(ctype == 'celcius'){
 			chartData.push(response.forecast.forecastday[indx].hour[9].temp_c);
 			chartData.push(response.forecast.forecastday[indx].hour[12].temp_c);
 			chartData.push(response.forecast.forecastday[indx].hour[15].temp_c);
@@ -183,6 +244,14 @@ function setChartData(weather, ctype, indx){
 			chartData.push(response.forecast.forecastday[indx].hour[21].temp_c);
 			chartData.push(response.forecast.forecastday[indx].hour[0].temp_c);
 			chartData.push(response.forecast.forecastday[indx].hour[6].temp_c);
+		}else if(ctype == 'fah'){
+			chartData.push(response.forecast.forecastday[indx].hour[9].temp_f);
+			chartData.push(response.forecast.forecastday[indx].hour[12].temp_f);
+			chartData.push(response.forecast.forecastday[indx].hour[15].temp_f);
+			chartData.push(response.forecast.forecastday[indx].hour[18].temp_f);	
+			chartData.push(response.forecast.forecastday[indx].hour[21].temp_f);
+			chartData.push(response.forecast.forecastday[indx].hour[0].temp_f);
+			chartData.push(response.forecast.forecastday[indx].hour[6].temp_f);
 		}else if(ctype == 'humidity'){
 			chartData.push(response.forecast.forecastday[indx].hour[9].humidity);
 			chartData.push(response.forecast.forecastday[indx].hour[12].humidity);
@@ -213,49 +282,75 @@ function getDaysToFollow(weather, indx){
 		let day3 = response.forecast.forecastday[2];
 		
 		let day = document.querySelectorAll('.text-ff-day');
-		let temp = document.querySelectorAll('.text-temp');
+		let cel = document.querySelectorAll('.text-cel');
+		let fah = document.querySelectorAll('.text-fah');
 		let img = document.querySelectorAll('.days-box img');
 
 		day[0].innerHTML = getDayText(new Date(day1.date).getDay()).slice(0,3);
 		img[0].src = day1.day.condition.icon;
-		temp[0].innerHTML =  day1.day.avgtemp_c + '°C';
+		cel[0].innerHTML =  day1.day.avgtemp_c + '°C';
+		fah[0].innerHTML =  day1.day.avgtemp_f + '°F';
+		
 		let first = document.querySelector('div[data-day="first"]');
 		first.onclick = () => {
 			displayWeatherInfo(weather);
-			createChart(weather, 'temp', 0, 0);
-			createChart(weather, 'humidity', 0, 1);
-			createChart(weather, 'wind', 0, 2);	
+			createChart(weather, 'celcius', 0, 0);
+			createChart(weather, 'fah', 0, 1);
+			createChart(weather, 'humidity', 0, 2);
+			createChart(weather, 'wind', 0, 3);	
+			setTimeout(() => changeChartDisplay(), 0);
 		};
 
 		day[1].innerHTML = getDayText(new Date(day2.date).getDay()).slice(0,3);
 		img[1].src = day2.day.condition.icon;
-		temp[1].innerHTML =  day2.day.avgtemp_c + '°C';
+		cel[1].innerHTML =  day2.day.avgtemp_c + '°C';
+		fah[1].innerHTML =  day2.day.avgtemp_f + '°F';
 		let second = document.querySelector('div[data-day="second"]');
 		second.onclick = () => {
 			displayValueOnOtherDay(weather, 1);
-			createChart(weather, 'temp', 1, 0);
-			createChart(weather, 'humidity', 1, 1);
-			createChart(weather, 'wind', 1, 2);
+			createChart(weather, 'celcius', 1, 0);
+			createChart(weather, 'fah', 1, 1);
+			createChart(weather, 'humidity', 1, 2);
+			createChart(weather, 'wind', 1, 3);
+			setTimeout(() => changeChartDisplay(), 0);
 		};
 
 		day[2].innerHTML = getDayText(new Date(day3.date).getDay()).slice(0,3);
 		img[2].src = day3.day.condition.icon;
-		temp[2].innerHTML =  day3.day.avgtemp_c + '°C';
+		cel[2].innerHTML =  day3.day.avgtemp_c + '°C';
+		fah[2].innerHTML =  day3.day.avgtemp_f + '°F';
 		let third = document.querySelector('div[data-day="third"]');
 		third.onclick = () => {
 			displayValueOnOtherDay(weather, 2);
-			createChart(weather, 'temp', 2, 0);
-			createChart(weather, 'humidity', 2, 1);
-			createChart(weather, 'wind', 2, 2);
+			createChart(weather, 'celcius', 2, 0);
+			createChart(weather, 'fah', 2, 1);
+			createChart(weather, 'humidity', 2, 2);
+			createChart(weather, 'wind', 2, 3);
+			setTimeout(() => changeChartDisplay(), 0);
 		};
 	});
 }
 
+function changeChartDisplay(){
+	let celChartDisplay = document.querySelector('#celcius-chart');
+	let celChartTranslatePosition = document.querySelector('#celcius-chart').style.transform;
+	let fahChartDisplay = document.querySelector('#fah-chart');
+	let fahChartTranslatePosition = document.querySelector('#fah-chart').style.transform;
+	let degreeType = document.querySelector('.type');
+	if(degreeType.innerHTML == '°C'){
+		celChartDisplay.style.cssText = `display: block !important; transform: ${celChartTranslatePosition}`;
+		fahChartDisplay.style.cssText = `display: none !important; transform: ${fahChartTranslatePosition}`;
+	}else{
+		celChartDisplay.style.cssText = `display: none !important; transform: ${celChartTranslatePosition}`;
+		fahChartDisplay.style.cssText = `display: block !important; transform: ${fahChartTranslatePosition}`;	
+	} 
+}
 
 function displayValueOnOtherDay(weather, indx){
 	weather.then(
 		(response) => {
-			let txtDegree = document.querySelector('.text-degree span');
+			let txtCelcius = document.querySelector('.text-degree .cel');
+			let txtFah = document.querySelector('.text-degree .fah');
 			let txtPrecep = document.querySelector('.text-precep');
 			let txtHumidity = document.querySelector('.text-humidity');
 			let txtWind = document.querySelector('.text-wind');
@@ -264,7 +359,8 @@ function displayValueOnOtherDay(weather, indx){
 			let txtWeather = document.querySelector('.text-weather');
 			let weatherImg = document.querySelector('.text-degree img');
 			
-			txtDegree.innerHTML  = response.forecast.forecastday[indx].day.avgtemp_c + '°C';
+			txtCelcius.innerHTML  = response.forecast.forecastday[indx].day.avgtemp_c + '°C';
+			txtFah.innerHTML  = response.forecast.forecastday[indx].day.avgtemp_f + '°F';
 			weatherImg.src = response.forecast.forecastday[indx].day.condition.icon;
 			txtPrecep.innerHTML  = 'Precipation: ' + response.forecast.forecastday[indx].day.totalprecip_in + '%';
 			txtHumidity.innerHTML  = 'Humidity: ' + response.forecast.forecastday[indx].day.avghumidity + '%';
